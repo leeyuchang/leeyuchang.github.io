8fc8b17a2ec14ca3dbe531a036f994eb16c960e2
leeyucha <leeyucha@gmail.com>
2020-10-04 22:46:20 +0900

    fetch test

--- src/model/ListModel.js
+++ src/model/ListModel.js
@@ -1,6 +1,6 @@
 export default {
   async getData(dataURL) {
-    const res = await fetch("//192.168.100.101:8080/list");
+    const res = await fetch("http://192.168.100.101:8080/list");
     if (!res.ok) {
       throw new Error("error");
     }
