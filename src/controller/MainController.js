f2468835a67314d913ca7b81979953e8067445f6
leeyucha <leeyucha@gmail.com>
2020-10-07 23:06:06 +0900

    clean code

--- src/controller/MainController.js
+++ src/controller/MainController.js
@@ -17,26 +17,26 @@ export default {
   init() {
 
     AddView.setup(document.getElementById('addView'))
-      .on('@submit', data => this.onRegist(data.detail));
+      .on('@submit', data => this.onRegist({ detail }));
 
     SignupView.setup(document.getElementById('signupView'))
-      .on('@submit', data => this.onSignup(data.detail));
+      .on('@submit', data => this.onSignup({ detail }));
 
     PageView.setup(document.getElementById('pageView'))
-      .on('@click', data => this.onPageNum(data.detail));
+      .on('@click', data => this.onPageNum({ detail }));
 
     LinkView.setup(document.getElementById('linkview'))
-      .on('@edit', data => this.onEditBtn(data.detail)) // rendering view : retrive data
-      .on('@del', data => this.onDelBtn(data.detail)); //  rendering view : delete  data
+      .on('@edit', data => this.onEditBtn({ detail })) // rendering view : retrive data
+        .on('@del', data => this.onDelBtn({ detail })); //  rendering view : delete  data
 
     EditView.setup(document.getElementById('editView'))
-      .on('@submit', data => this.onSubmitEdit(data.detail));
+      .on('@submit', data => this.onSubmitEdit({ detail }));
     
     DelView.setup(document.getElementById('delView'))
-      .on('@confirm', data => this.onConfirmDel(data.detail)); 
+      .on('@confirm', data => this.onConfirmDel({ detail })); 
     
     LoginView.setup(document.getElementById('loginView'))
-      .on('@login', data => this.onLogin(data.detail));
+      .on('@login', data => this.onLogin({ detail }));
 
 
     document.getElementById('signupBtn').addEventListener('click', () => SignupView.render());
@@ -47,55 +47,65 @@ export default {
   },
 
   onEditBtn(id) {
-    LinkModel.getOne(`http://localhost:8080/links/${id}`).then(data => EditView.render(data));
+    LinkModel
+      .getOne(`http://localhost:8080/links/${id}`)
+      .then(EditView.render);
   },
 
   onSubmitEdit(data) {
-    log(TAG,'onSubmitEdit:', data);
-    LinkModel.put(`http://localhost:8080/links/${data.id}`, data).then(() => {
-      $('#editModal').modal('toggle');
-      this.onPageNum(0);
-    });
+    LinkModel
+      .put(`http://localhost:8080/links/${data.id}`, data)
+      .then(() => {
+        $('#editModal').modal('toggle');
+        this.onPageNum(0);
+      });
   },
 
   onConfirmDel(id) {
-    log(TAG, 'onConfirmDel:', id);
-    LinkModel.delete(`http://localhost:8080/links/${id}`).then(result => {
-        this.onPageNum(0);
-      });
+    LinkModel
+      .delete(`http://localhost:8080/links/${id}`)
+      .then(result => {this.onPageNum(0);});
   },
 
   onDelBtn(data) { 
     DelView.render(data);
   },
 
-  onPageNum(page) { // TODO FIX ME API ADDRESS
-    LinkModel.get('http://localhost:8080/list?', page).then(data => {
-      LinkView.render(data);
-      PageView.render(data);
-    });
+  onPageNum(page) {
+    LinkModel
+      .get(`http://localhost:8080/list?`, page)
+      .then(data => {
+        LinkView.render(data);
+        PageView.render(data);
+      });
   },
 
   onRegist(data) {
-    LinkModel.post('http://localhost:8080/links', data)
-      .then(data => $('#addModal').modal('hide'))
-      .then(() => alert('Regist Completed'));
+    LinkModel
+      .post(`http://localhost:8080/links`, data)
+      .then(data => {
+        $('#addModal').modal('toggle');
+        alert('Regist Completed')
+      });
   },
 
   onSignup(data) {
-    SignupModel.post('http://localhost:8080/signup', data)
+    SignupModel
+      .post(`http://localhost:8080/signup`, data)
       .then(data => {
         localStorage.setItem('jwttoken', data.jwttoken);
         $('#signupModal').modal('toogle');
-      })
-      .then(() => alert('Signup Completed'));
+        alert('Signup Completed');
+      });
   },
 
   onLogin(data) {
-    LoginModel.post(`http://localhost:8080/login`, data).then((data) => {
-      $('#loginModal').modal('toggle');
-      log(TAG, 'onLogin ', data);
-      localStorage.setItem('jwttoken', data.jwttoken);
-    });
+    LoginModel
+      .post(`http://localhost:8080/login`, data)
+      .then((data) => {
+        $('#loginModal').modal('toggle');
+        log(TAG, 'onLogin ', data);
+        localStorage.setItem('jwttoken', data.jwttoken);
+      });
   },
 }
