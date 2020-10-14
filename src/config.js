495246988eb08c2dc7f573932971f18eabbf6902
leeyucha <leeyucha@gmail.com>
2020-10-14 14:01:41 +0900

    make it better

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-export const PAGE_SIZE='2'
-// export const BASE_URL='https://weblink001.news'
-export const BASE_URL='http://www.localhost:8080'
+export const PAGE_SIZE='10'
+export const BASE_URL='https://weblink001.news'
+// export const BASE_URL='http://www.localhost:8080'
 export const JWT ='jwttoken'
 
 
